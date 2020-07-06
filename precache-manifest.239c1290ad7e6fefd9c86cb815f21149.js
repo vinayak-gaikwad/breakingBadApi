8d00b8fa407e134ea4dd0539ce5bd0ece124a819
vinayak-gaikwad <58037628+vinayak-gaikwad@users.noreply.github.com>
2020-07-06 21:55:57 +0530

    Updates

---
+++ precache-manifest.239c1290ad7e6fefd9c86cb815f21149.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efa4a7f5ad288cd6e49b557f7e04722",
+    "url": "/breakingBadApi/index.html"
+  },
+  {
+    "revision": "ecd212e23b8d695bed18",
+    "url": "/breakingBadApi/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c79c02cc552bdb839a3a",
+    "url": "/breakingBadApi/static/css/main.42becae2.chunk.css"
+  },
+  {
+    "revision": "ecd212e23b8d695bed18",
+    "url": "/breakingBadApi/static/js/2.beb3b221.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingBadApi/static/js/2.beb3b221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79c02cc552bdb839a3a",
+    "url": "/breakingBadApi/static/js/main.fa83d532.chunk.js"
+  },
+  {
+    "revision": "d7e9f0beab07a97211c2",
+    "url": "/breakingBadApi/static/js/runtime-main.45339e3b.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakingBadApi/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingBadApi/static/media/logo.3795f66d.png"
+  }
+]);
